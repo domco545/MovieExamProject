40016742cad5ebb38d667320325e6e775dfe8fa2
ArmNem <58069062+ArmNem@users.noreply.github.com>
2020-01-06 11:46:53 +0100

    be

--- src/movieexamproject/be/Movie.java
+++ src/movieexamproject/be/Movie.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package movieexamproject.be;
-
+import java.sql.Timestamp;
 /**
  *
  * @author narma
@@ -15,6 +15,55 @@ public class Movie {
     private String name;
     private float rating;
     private String filelink;
-    private 
+    private Timestamp lastview;
+
+    public Movie(int id, String name, float rating, String filelink, Timestamp lastview)
+    {
+      this.id=id;
+      this.name= name;
+      this.rating=rating;
+      this.filelink=filelink;
+      this.lastview=lastview;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getFilelink() {
+        return filelink;
+    }
+
+    public void setFilelink(String filelink) {
+        this.filelink = filelink;
+    }
+
+    public Timestamp getLastview() {
+        return lastview;
+    }
+
+    public void setLastview(Timestamp lastview) {
+        this.lastview = lastview;
+    }
+    
     
 }
