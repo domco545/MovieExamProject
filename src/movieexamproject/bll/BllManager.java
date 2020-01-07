99fc81a2d8826894f8a175bdd120f6eec6dd962f
Truncatedshark <56824915+Truncatedshark@users.noreply.github.com>
2020-01-07 11:11:52 +0100

    Create BllManager.java

---
+++ src/movieexamproject/bll/BllManager.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieexamproject.bll;
+
+import java.util.ArrayList;
+import movieexamproject.be.Movie;
+import movieexamproject.dal.CategoryDBDAO;
+import movieexamproject.dal.MovieDBDAO;
+
+/**
+ *
+ * @author Martin
+ */
+public class BllManager {
+     private MovieDBDAO moviedao = new MovieDBDAO();
+     private CategoryDBDAO categorydbdao = new CategoryDBDAO();
+     
+     /*
+     public ArrayList<Movie> addSong(){
+     
+     }
+*/
+     /*
+     public ArrayList<Category> addCategory(){
+         
+     }
+*/
+}
