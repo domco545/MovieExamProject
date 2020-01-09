6d809b8a13b94fb4bdfedb2b029fea77352960b3
ArmNem <58069062+ArmNem@users.noreply.github.com>
2020-01-09 10:00:13 +0100

    Update BLLManager.java

--- src/movieexamproject/bll/BLLManager.java
+++ src/movieexamproject/bll/BLLManager.java
@@ -6,34 +6,26 @@
 package movieexamproject.bll;
 
 import java.util.ArrayList;
-import java.util.List;
-import movieexamproject.be.Category;
+import movieexamproject.be.Movie;
 import movieexamproject.dal.CategoryDBDAO;
 import movieexamproject.dal.MovieDBDAO;
 
 /**
  *
- * @author XMdag
+ * @author Martin
  */
-public class BLLManager implements Interface{
-    
-    CategoryDBDAO cat= new CategoryDBDAO();
-    MovieDBDAO mov= new MovieDBDAO();
-    List<Category> categorylist = new ArrayList();
-
-    public BLLManager() {
-        init();
-    }
-    
-
-    @Override
-    public List<Category> getAllCatergories() {
-        return categorylist;
-    }
-    
-    public void init()
-    {   
-        categorylist = cat.getAllCatergories();
-    }
-    
+public class BllManager {
+     private MovieDBDAO moviedao = new MovieDBDAO();
+     private CategoryDBDAO categorydbdao = new CategoryDBDAO();
+     
+     /*
+     public ArrayList<Movie> addSong(){
+     
+     }
+*/
+     /*
+     public ArrayList<Category> addCategory(){
+         
+     }
+*/
 }
