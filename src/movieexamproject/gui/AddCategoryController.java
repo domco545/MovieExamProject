e361dba04caadc7638f9e9da6f61b9b1c187c369
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-01-10 10:22:10 +0100

    fixed new movie and finished new category

--- src/movieexamproject/gui/AddCategoryController.java
+++ src/movieexamproject/gui/AddCategoryController.java
@@ -7,7 +7,15 @@ package movieexamproject.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import movieexamproject.bll.BllManager;
+import movieexamproject.bll.Interface;
 
 /**
  * FXML Controller class
@@ -16,6 +24,17 @@ import javafx.fxml.Initializable;
  */
 public class AddCategoryController implements Initializable {
 
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private TextField nameText;
+    @FXML
+    private Button addBtn;
+    @FXML
+    private Button cancelBtn;
+
+    Interface in = new BllManager();
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +42,20 @@ public class AddCategoryController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void addCategory(ActionEvent event) {
+        if(nameText.getText() == null || nameText.getText().trim().isEmpty()){
+            errorLabel.setText("Please set the name");
+        }else{
+            in.addCategory(nameText.getText());
+            cancel(event);
+        }
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
     
 }
