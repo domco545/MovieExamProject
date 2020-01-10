e1911ace1cefc193bf551af1c1544813399ecf81
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-01-10 12:52:12 +0100

    added remove movie

---
+++ src/movieexamproject/gui/RemoveMovieController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieexamproject.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import movieexamproject.be.Movie;
+import movieexamproject.bll.BllManager;
+import movieexamproject.bll.Interface;
+
+/**
+ * FXML Controller class
+ *
+ * @author domin
+ */
+public class RemoveMovieController implements Initializable {
+
+    @FXML
+    private Button deleteBtn;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Label nameLbl;
+
+    Interface in = new BllManager();
+    private Movie movie;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void delete(ActionEvent event) {
+        in.deleteMovie(movie.getId());
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+    
+    public void acceptMovie(Movie movie){
+        this.movie = movie;
+    }
+    
+}
