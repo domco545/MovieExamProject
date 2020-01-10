fea984c908913c986a6a9c44944af3a2b84d10fd
Truncatedshark <56824915+Truncatedshark@users.noreply.github.com>
2020-01-10 12:57:40 +0100

    Update FXMLDocumentController.java

--- src/movieexamproject/gui/FXMLDocumentController.java
+++ src/movieexamproject/gui/FXMLDocumentController.java
@@ -93,10 +93,10 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void playBtnAction(ActionEvent event) throws IOException {
         
-       String path = tableView.getSelectionModel().getSelectedItem().getFilelink();
+      // String path = tableView.getSelectionModel().getSelectedItem().getFilelink();
 
         
-        File file = new File(path);
+        File file = new File("C:/Users/XMdag/Downloads/kungfury.mp4");
         
         //first check if Desktop is supported by Platform or not
         if(!Desktop.isDesktopSupported()){
